8569c875ffddee2ebc41699bf9ece26bed475cc5
ravibhojani1604 <ravibhojani005@gmail.com>
2024-12-25 18:57:11 +0530

    feature 1

--- src/custom.js
+++ src/custom.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react'
 
 function CustomHook() {
-    console.log("avjkehl")
-    console.log("avjkehl")
 
-    const [list, setList]  = useState([])
+    const [list, setList]  = useState([]);
 
 
     const getListData = async () => {
